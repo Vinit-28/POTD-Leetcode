b8b33e1620d32e97d83f60b181cc5095922ac736
Vinit-28 <codervinit@gmail.com>
2023-09-01 23:46:47 +0530

    Solved Counting Bits

---
+++ Year=2023/Month=09/Date=01/Solution.cpp
@@ -0,0 +1,15 @@
+// Problem Link : https://leetcode.com/problems/counting-bits/
+
+
+// Solution -- Recursion + Memoization (DP) //
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n+1);
+        ans[0] = 0;
+        for(int i=1;i<=n;i++){
+            ans[i] = ans[i/2] + (i&1);
+        }
+        return ans;
+    }
+};
