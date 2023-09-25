72b80c228a98a7981bf302a487394b32dcdade7d
Vinit-28 <codervinit@gmail.com>
2023-09-26 00:25:22 +0530

    Solved Find the Difference

---
+++ Year=2023/Month=09/Date=25/Solution.cpp
@@ -0,0 +1,17 @@
+// Problem Link : https://leetcode.com/problems/find-the-difference/description/
+
+
+// Solution //
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        
+        int sum_s=0, sum_t=0;
+        
+        for(auto &ch : s) sum_s += int(ch);
+        
+        for(auto &ch : t) sum_t += int(ch);
+        
+        return char(sum_t-sum_s);
+    }
+};
