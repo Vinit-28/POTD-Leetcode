a057a03fab139f8f3ced8b05ff0ec4c14d3d21ae
Vinit-28 <codervinit@gmail.com>
2023-10-01 21:39:17 +0530

    Solved Reverse Words in a String III

---
+++ Year=2023/Month=10/Date=01/Solution.cpp
@@ -0,0 +1,18 @@
+// Problem Link : https://leetcode.com/problems/reverse-words-in-a-string-iii/description/
+
+
+// Solution //
+class Solution {
+public:
+    string reverseWords(string s) {
+        int start = 0, n = s.length();
+        for(int i=0;i<n;i++){
+            if( s[i] == ' ' ){
+                reverse(s.begin()+start, s.begin()+i);
+                start = i+1;
+            }
+        }
+        reverse(s.begin()+start, s.end());
+        return s;
+    }
+};
