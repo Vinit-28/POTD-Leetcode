162533f241e6d531a25269946ab9a4232e2f940d
Vinit-28 <codervinit@gmail.com>
2023-11-15 23:37:28 +0530

    Solved Maximum Element After Decreasing and Rearranging

---
+++ Year=2023/Month=11/Date=15/Solution.cpp
@@ -0,0 +1,18 @@
+// Problem Link : https://leetcode.com/problems/maximum-element-after-decreasing-and-rearranging/description/
+
+
+// Solution //
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        arr[0] = 1;
+        int n = arr.size();
+        for(int i=1;i<n;i++){
+            if( (arr[i]-arr[i-1]) > 1 ){
+                arr[i] = arr[i-1] + 1;
+            }
+        }
+        return arr[n-1];
+    }
+};
