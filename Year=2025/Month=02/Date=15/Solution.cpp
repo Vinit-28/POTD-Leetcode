15114c85d9cb0209c339d24280877296a2d58af8
Vinit-28 <codervinit@gmail.com>
2025-02-17 00:11:49 +0530

    Solved Find the Punishment Number of an Integer

---
+++ Year=2025/Month=02/Date=15/Solution.cpp
@@ -0,0 +1,28 @@
+// Problem Link : https://leetcode.com/problems/find-the-punishment-number-of-an-integer/description/
+
+
+// Solution //
+class Solution {
+public:
+    int punishmentNumber(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (canPartition(to_string(i * i), 0, i)) {
+                sum += (i * i);
+            }
+        }
+        return sum;
+    }
+
+    bool canPartition(string num, int index, int target) {
+        if (index == num.length()) return target == 0;
+
+        int sum = 0;
+        for (int i = index; i < num.length(); i++) {
+            sum = sum * 10 + (num[i] - '0');
+            if (sum > target) break;
+            if (canPartition(num, i + 1, target - sum)) return true;
+        }
+        return false;
+    }
+};
