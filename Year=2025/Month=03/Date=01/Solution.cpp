4a141e9ee7b15601e735e20bb2675c5d6f1007ba
Vinit-28 <codervinit@gmail.com>
2025-03-01 22:59:06 +0530

    Solved Apply Operations to an Array

---
+++ Year=2025/Month=03/Date=01/Solution.cpp
@@ -0,0 +1,21 @@
+// Problem Link : https://leetcode.com/problems/apply-operations-to-an-array/description/
+
+
+// Solution //
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        int n = nums.size();
+        for(int i=0; i<n-1; i++){
+            if( nums[i] == nums[i+1] ){
+                nums[i] *= 2;
+                nums[i+1] = 0;
+            }
+        }
+        int idx = 0;
+        for(int i=0; i<n; i++){
+            if( nums[i] > 0 ) swap(nums[idx++], nums[i]);
+        }
+        return nums;
+    }
+};
